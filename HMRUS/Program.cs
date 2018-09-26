5fa0db253e0116ca50b415032a7fc53fe910d685
Robert Tublen <robert.tublen@junction7.se>
2018-09-26 19:34:29 +0200

    Add project files.

---
+++ HMRUS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HMRUS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
